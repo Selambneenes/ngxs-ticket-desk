f10b69563729d49690b492790472f0d9e162c9ca
Enes Tolgay <tolgayenes5@gmail.com>
2026-06-12 15:32:29 +0300

    Toast library added, the library has been implemented. The entire design is completed, and all features have been successfully implemented using Signal.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+    }),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
--- src/app/tickets/ticket-item/ticket-item.ts
+++ src/app/tickets/ticket-item/ticket-item.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Ticket } from '../tickets';
 
 @Component({
@@ -8,5 +8,22 @@ import { Ticket } from '../tickets';
   styleUrl: './ticket-item.css',
 })
 export class TicketItem {
-  @Input() ticket!: Ticket; // 'any' yerine kendi Ticket tipini/arayüzünü yazabilirsin
+  @Input() ticket!: Ticket;
+  @Output() removeTicket = new EventEmitter<number>();
+  @Output() updateStatus = new EventEmitter<{ id: number; newStatus: Ticket['status'] }>();
+
+  onStatusChange($event: Event) {
+    const select = $event.target as HTMLSelectElement;
+    const value = select.value;
+
+    if (value === this.ticket.status || !['Open', 'In Progress', 'Closed'].includes(value)) {
+      console.log('Invalid status selected');
+      return;
+    }
+
+    const correctTypeValue: Ticket['status'] = value as Ticket['status'];
+
+    this.updateStatus.emit({ id: this.ticket.id, newStatus: correctTypeValue });
+    console.log(`Status updated for ticket ${this.ticket.id}: ${value}`);
+  }
 }
